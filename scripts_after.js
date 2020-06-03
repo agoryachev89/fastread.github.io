5d9d58099df6c43304c32759921b289bc55b19b7
Goryachev Alexey <aleksei.goriachev@academmedia.com>
2020-06-03 19:29:47 +0700

    func

--- scripts_after.js
+++ scripts_after.js
@@ -33,23 +33,53 @@ function report(state) {
 
 handlePermission();
 
+var i = 0;
+var wps = 300;
+var speed = 200;
+
+function changeWPS(delta) {
+    wps += delta
+    speed = 60000 / wps
+    document.getElementById('wps').innerHTML = wps
+}
+
+function renderText(text) {
+  setTimeout(function() {
+    word = text.split(' ')[i]
+    document.getElementById('word').innerHTML = word
+    i++;
+    if (i < text.split(' ').length) {
+      renderText(text);
+    } else {
+        i = 0
+    }
+  }, speed)
+}
+
+
 function clipboardPaste() {
     if (navigator.clipboard.readText) {
         navigator.clipboard.readText()
         .then(text => {
             console.log('Pasted content: ', text);
             document.getElementById("content").innerHTML = text;
+            renderText(text);
         })
         .catch(err => {
             console.error('Failed to read clipboard contents: ', err);
         });
     } else {
+        // не работает в мозиле, мб через экстеншен проканает
+        document.designMode = "on"
         var pasteText = document.querySelector("#output");
         pasteText.contentEditable = true;
         pasteText.focus();
         document.execCommand("paste");
         console.log('Pasted content: ', pasteText.textContent);
         document.getElementById("content").innerHTML = pasteText.textContent;
+        var text = pasteText.textContent;
+        renderText(text);
     }
 }
+document.getElementById('wps').innerHTML = wps
 document.querySelector("#paste").addEventListener("click", clipboardPaste);
